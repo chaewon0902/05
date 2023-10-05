ee4dba753ec1a542a6f14a6c3bfd77466c8c5022
chaewon0902 <144991565+chaewon0902@users.noreply.github.com>
2023-10-05 16:18:25 +0900

    3

--- main.c
+++ main.c
@@ -5,16 +5,17 @@
 
 int main(int argc, char *argv[]) 
 {
-	int x;
-	printf("정수 하나를 입력하시오: ");
-	scanf("%d",&x);
+	int num = 0;
+	char c;
 	
-	if(x>=0){
-		printf("절대값은 %d입니다.\n",x); //조건식이 맞으면 실행 
-	}
-	else{
-		printf("절대값은 %d입니다.\n",-x); //조건식이 틀리면 실행 
-	}
+	printf("input a string : ");
+	
+	while ((c=getchar())!= '\n'){
+		if(c>='0' && c<='9'){  //c가 숫자이면 
+			num++; //num=num+1 
+		}
+}
+	printf("The number of digits is %d\n", num);
 	
 	return 0;
 }
