d6316bdc4f933577fdd14c172bb60f93323ad316
chaewon0902 <144991565+chaewon0902@users.noreply.github.com>
2023-10-05 15:48:28 +0900

    2

--- main.c
+++ main.c
@@ -9,14 +9,11 @@ int main(int argc, char *argv[])
 	printf("정수 하나를 입력하시오: ");
 	scanf("%d",&x);
 	
-	if(x>0){
-		printf("양수입니다.\n");
-	}
-	else if(x<0){
-		printf("음수입니다.\n");
+	if(x>=0){
+		printf("절대값은 %d입니다.\n",x); //조건식이 맞으면 실행 
 	}
 	else{
-		printf("0입니다.\n");
+		printf("절대값은 %d입니다.\n",-x); //조건식이 틀리면 실행 
 	}
 	
 	return 0;
